c09376052c6faa864ae7f4af47719a499e6a172c
nairbdev2 <bdevil2@uic.edu>
2019-09-12 14:40:17 -0500

    Added Scripts for Button

---
+++ Projects/Project 1/scripts/BackVirtualButtons_Script.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class BackVirtualButtons_Script : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    VirtualButtonBehaviour[] virtualButtonBehaviours;
+
+    //Variables
+    string virButton;
+    //Three Buttons
+    public GameObject BookReviewButton;
+    public GameObject VideoReviewButton;
+    public GameObject MapButton;
+
+    //Three Objects to Hide
+    public GameObject Map_GroupObj;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Register all the buttons
+        //virtualButtonBehaviours = GetComponentsInChildren<VirtualButtonBehaviour>();
+
+        //for (int i = 0; i < virtualButtonBehaviours.Length; i++)
+        //{
+        //    virtualButtonBehaviours[i].RegisterEventHandler(this);
+        //}
+
+        BookReviewButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        VideoReviewButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        MapButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        virButton = vb.VirtualButtonName;
+
+        if (virButton.Equals("BookReviewButton"))
+        {
+            Map_GroupObj.SetActive(false);
+            Debug.Log("BookReviewButton was pressed.");
+        }
+        else if (virButton.Equals("VideoReviewButton"))
+        {
+            Map_GroupObj.SetActive(false);
+            Debug.Log("VideoReviewButton was pressed.");
+        }
+        else if (virButton.Equals("MapButton"))
+        {
+            Map_GroupObj.SetActive(true);
+            Debug.Log("MapButton was pressed.");
+        } else
+        {
+
+        }
+        
+
+        Debug.Log("Button Pressed");
+        Debug.Log("What is this: " + virButton);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        
+        Debug.Log("Button Released");
+    }
+
+
+}
