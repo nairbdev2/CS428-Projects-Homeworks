1ca4787c1a8cdf8c256805618fcd40a4d921dde8
nairbdev2 <bdevil2@uic.edu>
2019-09-12 00:18:04 -0500

    Added Scene and scripts for AR Button

---
+++ Projects/Project 1/scripts/VB_Script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VB_Script : MonoBehaviour, IVirtualButtonEventHandler 
+{
+    public GameObject vbBtnObj;
+    public GameObject spokePerson;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb) {
+        spokePerson.SetActive(true);
+        Debug.Log("Button Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {
+        spokePerson.SetActive(false);
+        Debug.Log("Button Released");
+    }
+
+
+}
