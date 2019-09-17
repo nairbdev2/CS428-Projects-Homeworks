2eb6b9d467e9842b876c627335151fc6209099e6
nairbdev2 <bdevil2@uic.edu>
2019-09-17 16:29:20 -0500

    Adjusted the Script where you press the VideoButton or BookButton and they would cycle through each other.

--- Projects/Project 1/Assets/scripts/BackVirtualButtons_Script.cs
+++ Projects/Project 1/Assets/scripts/BackVirtualButtons_Script.cs
@@ -9,16 +9,24 @@ public class BackVirtualButtons_Script : MonoBehaviour, IVirtualButtonEventHandl
     VirtualButtonBehaviour[] virtualButtonBehaviours;
 
     //Variables
-    //string virButton;
-    //Three Buttons
-    //public GameObject BookReviewButton;
-    //public GameObject VideoReviewButton;
-    //public GameObject MapButton;
-
-    //Three Objects to Hide
     public GameObject Map_GroupObj;
-    
-    
+    public GameObject BookReview_GroupObj;
+    public GameObject VideoReview_GroupObj;
+    public GameObject Text_GroupObj;
+
+    //VideoObjects
+    public GameObject bdevil2Video;
+    public GameObject nhe3Video;
+    public GameObject yli265Video;
+
+    //BookObjects
+    public GameObject bdevil2Book;
+    public GameObject nhe3Book;
+    public GameObject yli265Book;
+
+    //Counter when to Cycle through
+    int BookCycleCounter;
+    int VideoCycleCounter;
 
 
     // Start is called before the first frame update
@@ -32,10 +40,9 @@ public class BackVirtualButtons_Script : MonoBehaviour, IVirtualButtonEventHandl
             virtualButtonBehaviours[i].RegisterEventHandler(this);
         }
 
-        //BookReviewButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
-        //VideoReviewButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
-        //MapButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
-
+        //Cyclec Counter
+        BookCycleCounter = 0;
+        VideoCycleCounter = 0;
 
     }
 
@@ -53,25 +60,85 @@ public class BackVirtualButtons_Script : MonoBehaviour, IVirtualButtonEventHandl
         if (stringSplit[0].Equals("BookReviewButton"))
         {
             Map_GroupObj.SetActive(false);
+            BookReview_GroupObj.SetActive(true);
+            Text_GroupObj.SetActive(false);
+            VideoReview_GroupObj.SetActive(false);
+
+            //Implement cycling through the Book Reviews 
+            if (BookCycleCounter == 1)
+            {
+                bdevil2Book.SetActive(true);
+                yli265Book.SetActive(false);
+                nhe3Book.SetActive(false);
+            }
+            else if (BookCycleCounter == 2)
+            {
+                bdevil2Book.SetActive(false);
+                yli265Book.SetActive(true);
+                nhe3Book.SetActive(false);
+            }
+            else if (BookCycleCounter == 3)
+            {
+                bdevil2Book.SetActive(false);
+                yli265Book.SetActive(false);
+                nhe3Book.SetActive(true);
+            }
+            else
+            {
+                BookCycleCounter = 0;
+            }
+
+            BookCycleCounter++;
+
+
+
             Debug.Log("BookReviewButton was pressed.");
         }
         else if (stringSplit[0].Equals("VideoReviewButton"))
         {
             Map_GroupObj.SetActive(false);
+            BookReview_GroupObj.SetActive(false);
+            Text_GroupObj.SetActive(false);
+            VideoReview_GroupObj.SetActive(true);
+
+            //Implement Cycling through the videos
+            if (VideoCycleCounter == 1)
+            {
+                bdevil2Video.SetActive(true);
+                yli265Video.SetActive(false);
+                nhe3Video.SetActive(false);
+            } else if (VideoCycleCounter == 2)
+            {
+                bdevil2Video.SetActive(false);
+                yli265Video.SetActive(true);
+                nhe3Video.SetActive(false);
+            } else if (VideoCycleCounter == 3)
+            {
+                bdevil2Video.SetActive(false);
+                yli265Video.SetActive(false);
+                nhe3Video.SetActive(true);
+            } else
+            {
+                VideoCycleCounter = 0;
+            }
+            
+            VideoCycleCounter++;
+
             Debug.Log("VideoReviewButton was pressed.");
         }
         else if (stringSplit[0].Equals("ShowMapButton"))
         {
             Map_GroupObj.SetActive(true);
+            BookReview_GroupObj.SetActive(false);
+            Text_GroupObj.SetActive(false);
+            VideoReview_GroupObj.SetActive(false);
             Debug.Log("MapButton was pressed.");
         } else
         {
-
+            Debug.Log("Nothing was read in.");
         }
         
-
         Debug.Log("Button Pressed");
-        //Debug.Log("What is this: " + virButton);
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
