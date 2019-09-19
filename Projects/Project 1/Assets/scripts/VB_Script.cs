b562ec791f35a0f0407fabae60eb3ac88cdad35d
nairbdev2@github.com <bdevil2@uic.edu>
2019-09-18 22:24:47 -0500

    Added new Script to adjust the button.

--- Projects/Project 1/Assets/scripts/VB_Script.cs
+++ Projects/Project 1/Assets/scripts/VB_Script.cs
@@ -8,13 +8,16 @@ public class VB_Script : MonoBehaviour, IVirtualButtonEventHandler
     public GameObject vbBtnObj;
     public GameObject spokePerson;
 
+    //Counter To Toggle 'On' or 'Off'
+    bool buttonToggle;
+
 
     // Start is called before the first frame update
     void Start()
     {
 
         vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
-
+        buttonToggle = false;
     }
 
     // Update is called once per frame
@@ -24,12 +27,27 @@ public class VB_Script : MonoBehaviour, IVirtualButtonEventHandler
     }
 
     public void OnButtonPressed(VirtualButtonBehaviour vb) {
-        spokePerson.SetActive(true);
+
+        string virButton = vb.ToString();
+        string[] stringSplit = virButton.Split(' ');
+
+        if (stringSplit[0].Equals("VB_Start_Button"))
+        {
+            if (buttonToggle == false)
+            {
+                buttonToggle = true;
+                spokePerson.SetActive(true);
+            } else
+            {
+                buttonToggle = false;
+                spokePerson.SetActive(false);
+            }
+        }
         Debug.Log("Button Pressed");
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb) {
-        spokePerson.SetActive(false);
+
         Debug.Log("Button Released");
     }
 
